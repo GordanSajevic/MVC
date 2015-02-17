b11315fe7069f1d6f3e273eacdb6ec357ba0e65b
Gordan Sajević <gsajevic@hotmail.com>
2015-02-17 22:03:31 +0100

    Update Contact.java

--- src/ba/bitcamp/model/Contact.java
+++ src/ba/bitcamp/model/Contact.java
@@ -5,12 +5,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedList;
 
+/*
+* Class for contacts in phonebook
+*/
+
 public class Contact extends Application {
 
 	private int id;
 	private String name, surname, number;
 	private final static String tableName = "contacts";
-
+/*
+* Constructor with no parameters
+*/
 	public Contact() {
 
 		this.id = -1;
@@ -19,7 +25,9 @@ public class Contact extends Application {
 		this.number = "";
 
 	}
-	
+/*
+* Constructor with three parameters.
+*/	
 	public Contact(int id, String name, String surname)
 	{
 		this.name = name;
@@ -27,7 +35,10 @@ public class Contact extends Application {
 		this.number = "";
 		this.id = id;
 	}
-
+/*
+* Constructor with three parameters.
+* Default value for id is -1
+*/
 	public Contact(String name, String surname, String number) {
 
 		this.id = -1;
@@ -35,7 +46,9 @@ public class Contact extends Application {
 		this.surname = surname;
 		this.number = number;
 	}
-
+/*
+* Constructor with all four parameters
+*/
 	public Contact(int id, String name, String surname, String number) {
 
 		this.id = id;
@@ -44,6 +57,12 @@ public class Contact extends Application {
 		this.number = number;
 	}
 
+/*
+* Method that returns contact with given id.
+* We call find method from Application class, and give it id and table name.
+* Than we put all values in strings, so we use them to create contact
+* and return that contact
+*/
 	public static Contact find(int id) {
 		ResultSet res = Application.find(id, tableName);
 		try {
@@ -57,12 +76,19 @@ public class Contact extends Application {
 			return null;
 		}
 	}
-	
+/*
+* Method that sends values in save method in Application class
+* and checks if save method is valid, and returns boolean value.
+*/
 	public boolean save(){
 		 String values = String.format("(?, '%s', '%s', '%s')", this.name, this.surname, this.number);
 		 return Application.save(tableName, values);
 	}
-	
+/*
+* Method that returns array with all contacts. First, we use all() method
+* from Application class to put all contacts in ResultSet. Then, we put all
+* contact in linked list. After that, we create array, and put all contacts in array.
+*/	
 	public static Contact[] all()
 	{
 		ResultSet rs = Application.all(tableName, "ID, name, surname");
@@ -87,41 +113,59 @@ public class Contact extends Application {
 			return new Contact[0];
 		}
 	}
-	
+/*
+* Getter for name and surname
+*/
 	public String getDisplayName()
 	{
 		return this.name + " " + this.surname;
 	}
-	
+/*
+* Method that creates array with all contacts
+*/	
 	public static void list()
 	{
 		Contact[] all = Contact.all();
 	}
-
+/*
+* Getter for id
+*/
 	public int getId() {
 		return id;
 	}
-
+/*
+* Getter for name
+*/
 	public String getName() {
 		return name;
 	}
-
+/*
+* Setter for name
+*/
 	public void setName(String name) {
 		this.name = name;
 	}
-
+/*
+* Getter for surname
+*/
 	public String getSurname() {
 		return surname;
 	}
-
+/*
+* Setter for surname
+*/
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
-
+/*
+* Getter for number
+*/
 	public String getNumber() {
 		return number;
 	}
-
+/*
+* Setter for number
+*/
 	public void setNumber(String number) {
 		this.number = number;
 	}
