e3426ef50c06b5b0909b8444010729b9a8b94910
Gordan Sajević <gsajevic@hotmail.com>
2015-02-17 21:38:17 +0100

    Update Main.java

--- src/ba/bitcamp/view/Main.java
+++ src/ba/bitcamp/view/Main.java
@@ -5,10 +5,16 @@ import javax.swing.JFrame;
 
 public class Main {
 
+/*
+* Class creates main frame for our application
+*/
+
 	private static JFrame window = null;
 	protected static int windowWidth = 400;
 	protected static int windowHeight = 500;
-
+/*
+* Method initializes main frame
+*/
 	public static void init() {
 		window = new JFrame("BitBook");
 		window.setSize(windowWidth, windowHeight);
@@ -16,13 +22,17 @@ public class Main {
 		window.setResizable(false);
 
 	}
-
+/*
+* Method sets visibility of main frame
+*/
 	protected static void setVisible() {
 		window.validate();
 		window.repaint();
 		window.setVisible(true);
 	}
-
+/*
+* Method sets panel for main frame
+*/
 	protected static void replaceContent(Container panel) {
 		window.setContentPane(panel);
 		setVisible();
